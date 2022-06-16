00c7770585548813dc487842fdcd9e2c4d55cfef
Max Kalahur <kalahur@teamharbour.com>
2022-06-16 20:33:13 +0300

    esbuild fix

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,9 +4,9 @@ const vuePlugin = require("esbuild-plugin-vue3");
 require("esbuild").build({
   entryPoints: ["application.js", "invoice_form.js"],
   bundle: true,
-  minify: false,
+  minify: true,
   outdir: path.join(process.cwd(), "app/assets/builds"),
   absWorkingDir: path.join(process.cwd(), "app/javascript"),
-  watch: true,
+  watch: false,
   plugins: [vuePlugin()],
 }).catch(() => process.exit(1));
